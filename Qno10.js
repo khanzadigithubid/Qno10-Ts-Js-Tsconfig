4201ccccb9b3d3562eb7326ee3356ba2321a1b9f
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:31:53 +0500

    Qno10 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno10 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno10.js
@@ -0,0 +1,15 @@
+"use strict";
+/* Adding Comments: Choose two of the programs you have written, and add atleast one
+comment to each.If you don't have anything specific to write because your programs are too
+ simple at this point, just add your name and the current date at the top of each program
+ file. Then, write one sentence describing what the program does.*/
+let a = 15;
+let b = 5;
+//Addition
+console.log(`${a}+${b}=${a + b}`);
+//Subtraction
+console.log(`${a}-${b}=${a - b}`);
+//Multiplication
+console.log(`${a}*${b}=${a * b}`);
+//Division
+console.log(`${a}/${b}=${a / b}`);
---
+++ Qno10.ts
@@ -0,0 +1,18 @@
+/* Adding Comments: Choose two of the programs you have written, and add atleast one 
+comment to each.If you don't have anything specific to write because your programs are too
+ simple at this point, just add your name and the current date at the top of each program 
+ file. Then, write one sentence describing what the program does.*/
+
+let a: number = 15;
+let b: number = 5;
+
+                   //Addition
+console.log(`${a}+${b}=${a+b}`);
+                //Subtraction
+console.log(`${a}-${b}=${a-b}`);
+                //Multiplication
+console.log(`${a}*${b}=${a*b}`);
+                 //Division
+console.log(`${a}/${b}=${a/b}`);
+
+
